cf05cac0d3a281e417d8ee437a40b7bf84f4098c
Isabella <idavila2021@gmail.com>
2024-07-23 19:27:29 +0100

    Deploy backend

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+
 var cors = require("cors");
 const router = require("./router.js");
 
@@ -9,6 +10,6 @@ app.use(cors());
 app.use(express.json());
 app.use(router);
 
-app.listen(PORT, function () {
+app.listen(PORT, function() {
   console.log(`Server @  http://localhost:${PORT}`);
 });
--- model.js
+++ model.js
@@ -8,7 +8,7 @@ load().catch((err) => console.log(err));
 
 const dataSchema = new mongoose.Schema({
   camera: { type: String, required: true },
-  url: { type: String, required: true }
+  url: { type: String, required: true },
 });
 
 const Model = mongoose.model("image", dataSchema);
