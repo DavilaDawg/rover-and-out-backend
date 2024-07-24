ad3c49da664f88e0a9f50836fc0fc8eeaa3d2901
Isabella <idavila2021@gmail.com>
2024-07-24 17:01:31 +0100

    Add env file for port

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 const express = require("express");
 const app = express();
+require('dotenv').config();
 
 var cors = require("cors");
 const router = require("./router.js");
 
-const PORT = 3000;
+const port = process.env.PORT || 3000; 
 
 app.use(cors());
 app.use(express.json());
 app.use(router);
 
-app.listen(PORT, function() {
-  console.log(`Server @  http://localhost:${PORT}`);
+app.listen(port, function() {
+  console.log(`Server @  http://localhost:${port}`);
 });
