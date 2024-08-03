ad147132375eceff5c24cb33a8dc1780b3511b94
Isabella <idavila2021@gmail.com>
2024-08-03 11:01:03 +0100

    fix router

--- router.js
+++ router.js
@@ -1,10 +1,6 @@
 const express = require("express");
-const router = Router();
+const router = express.Router();
 const control = require("./controller.js");
-
-router.get("/", (req, res) => {
-    res.send("Welcome to the API!");
-});
 
 router.get("/", (req, res) => {
     res.send("Welcome to the API!");
